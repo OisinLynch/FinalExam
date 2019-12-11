59ba8d18a32cb982d5bbc6f20e0bff583c23e752
S00189006 <S00189006@LAB-B1042-05>
2019-12-11 09:41:29 +0000

    Final Exam

--- FinalExam/MainWindow.xaml.cs
+++ FinalExam/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace FinalExam
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
---
+++ FinalExam/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam
+{
+    //Position enum
+    public enum Position { Goakeeper, Defender, Midfielder, Forward }
+    class Player
+    {
+        //Properties
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public Position PreferedPosition { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+    }
+}
